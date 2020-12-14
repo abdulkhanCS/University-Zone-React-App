4d33235f18494b0ff11b687cb9f6ec555d1252c7
Abdul Khan <abdulkhanCS@gmail.com>
2020-12-14 14:33:17 -0500

    fixes

--- src/components/loginScreen.js
+++ src/components/loginScreen.js
@@ -32,7 +32,7 @@ export default class loginScreen extends React.Component {
 
   async onSubmit(e) {
     const thisUsername = this.state.username;
-    Axios.get("http://localhost:3000/user/", {
+    Axios.get("/user/", {
       params: {
         username: thisUsername
       },
