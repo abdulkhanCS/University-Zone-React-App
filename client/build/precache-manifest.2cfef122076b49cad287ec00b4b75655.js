c178370bda4597deab8d9bb6442c8bf8981d618f
Abdul Khan <abdulkhanCS@gmail.com>
2020-12-21 15:31:38 -0500

    preparation for heroku deployment

---
+++ client/build/precache-manifest.2cfef122076b49cad287ec00b4b75655.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e7b0a977be5462d68ae45719a762972",
+    "url": "/co-sphere.github.io/index.html"
+  },
+  {
+    "revision": "d5efd07567bf8684bdd1",
+    "url": "/co-sphere.github.io/static/css/main.bc282503.chunk.css"
+  },
+  {
+    "revision": "3fa3e390a92365bd29b9",
+    "url": "/co-sphere.github.io/static/js/2.657c8935.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/co-sphere.github.io/static/js/2.657c8935.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5efd07567bf8684bdd1",
+    "url": "/co-sphere.github.io/static/js/main.4e928ff8.chunk.js"
+  },
+  {
+    "revision": "30fbb3ffd48e7e4e3ba6",
+    "url": "/co-sphere.github.io/static/js/runtime-main.deab44ce.js"
+  }
+]);
