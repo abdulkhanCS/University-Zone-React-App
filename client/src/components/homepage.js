2c98368e336a8225b83936d7c1fd81be9eae7531
Abdul Khan <abdulkhanCS@gmail.com>
2021-03-20 21:48:34 -0400

    responsitivity changes

--- client/src/components/homepage.js
+++ client/src/components/homepage.js
@@ -137,7 +137,7 @@ class home extends React.Component {
               id="profile-picture"
               src="https://img.rawpixel.com/s3fs-private/rawpixel_images/website_content/v246-tent-33-business_2.jpg?bg=transparent&con=3&cs=srgb&dpr=1&fm=jpg&ixlib=php-3.1.0&q=65&usm=15&vib=3&w=800&s=19ed9a41550e368fefefc653763e9497"
             />
-            <h2 id="profile-name">{this.state.userFirstName}'s homepage</h2>
+            <h2 id="profile-name">{this.state.userFirstName}</h2>
           </div>
           <div id="homepage-menu"></div>
         </div>
